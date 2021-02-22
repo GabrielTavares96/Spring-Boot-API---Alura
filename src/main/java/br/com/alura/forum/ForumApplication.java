689014999b35f48f957dfcafac484e41b359abb0
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-02-22 09:59:11 -0300

    Adicionado Spring Cache

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @Slf4j
 @EnableSpringDataWebSupport
+@EnableCaching
 public class ForumApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
