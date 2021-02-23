7acbe53d328d746387f457869a40caea549c5e82
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-02-23 14:40:43 -0300

    Ajuste Swagger

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -1,17 +1,16 @@
 package br.com.alura.forum.config.swagger;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import br.com.alura.forum.modelo.Usuario;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.ParameterType;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import static java.util.Collections.singletonList;
 
 
 @Configuration
@@ -25,13 +24,13 @@ public class SwaggerConfigurations {
                 .paths(PathSelectors.ant("/**"))
                 .build()
                 .ignoredParameterTypes(Usuario.class)
-                .globalOperationParameters(Arrays.asList(
-                        new ParameterBuilder()
+                .globalRequestParameters(
+                        singletonList(new springfox.documentation.builders.RequestParameterBuilder()
                                 .name("Authorization")
                                 .description("Header para token JWT")
-                                .modelRef(new ModelRef("string"))
-                                .parameterType("header")
+                                .in(ParameterType.HEADER)
                                 .required(false)
+                                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
                                 .build()));
     }
 
